1fb7c49207cbcab4dc623602e05fd2cc27bd071c
Minwoo <ymw1023@kakao.com>
2025-01-07 12:01:33 +0900

    [Silver I] Title: 단지번호붙이기, Time: 8 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -6,13 +6,9 @@
 
 using namespace std;
 
-vector<int> dfs(vector<vector<int>> &field, int N) {
-    vector<int> answer;
-    vector<vector<bool>> visit;
-    
-    for (int i = 0; i < N; i++) {
-        visit.push_back(vector<bool> (N));
-    }
+int dfs(const vector<vector<int>> &field, vector<vector<bool>>& visit, int N, int i, int j) {
+    // 연결된 단지의 수를 저장할 변수
+    int count = 0;
     
     vector<int> moveX = { 1, -1, 0, 0 };
     vector<int> moveY = { 0, 0, 1, -1 };
@@ -21,65 +17,35 @@ vector<int> dfs(vector<vector<int>> &field, int N) {
     int nextX = 0;
     int nextY = 0;
     
-    // 연결된 단지의 수를 저장할 변수
-    int count = 0;
-    
     // 스택에서 꺼내온 좌표 저장할 변수
     vector<int> point(2);
+            
+    stack<vector<int>> dfs_stack;
     
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            if(field[i][j] == 0 || visit[i][j]) {
-                continue;
+    dfs_stack.push({ i, j });
+            
+    while (!dfs_stack.empty()) {
+        point = dfs_stack.top();
+        dfs_stack.pop();
+        
+        if (visit[point[0]][point[1]]) {
+            continue;
+        }
+        
+        visit[point[0]][point[1]] = true;
+        count++;
+        
+        for (int k = 0; k < 4; k++) {
+            nextX = point[1] + moveX[k];
+            nextY = point[0] + moveY[k];
+            
+            if (nextX >= 0 && nextX < N && nextY >= 0 && nextY < N && field[nextY][nextX] == 1 && !visit[nextY][nextX]) {
+                dfs_stack.push({ nextY, nextX });
             }
-            
-            count = 0;
-            
-            stack<vector<int>> dfs_stack;
-            
-            dfs_stack.push({ i, j });
-            
-            while (!dfs_stack.empty()) {
-                point = dfs_stack.top();
-                dfs_stack.pop();
-                
-                if (visit[point[0]][point[1]]) {
-                    continue;
-                }
-                
-                visit[point[0]][point[1]] = true;
-                count++;
-                
-                for (int k = 0; k < 4; k++) {
-                    nextX = point[1] + moveX[k];
-                    nextY = point[0] + moveY[k];
-                    
-                    if (nextX >= 0 && nextX < N && nextY >= 0 && nextY < N && field[nextY][nextX] == 1 && !visit[nextY][nextX]) {
-                        dfs_stack.push({ nextY, nextX });
-                    }
-                }
-            }
-            
-            answer.push_back(count);
-            
-            /* 중간 결과 출력
-            for (int l = 0; l < N; l++) {
-                for (int m = 0; m < N; m++) {
-                    if (visit[l][m]) {
-                        cout << "T";
-                    }
-                    else {
-                        cout <<"F";
-                    }
-                }
-                cout << endl;
-            }
-            cout << endl;
-            */
         }
     }
     
-    return answer;
+    return count;
 }
 
 int main() {
@@ -102,7 +68,38 @@ int main() {
         }
     }
     
-    vector<int> answer = dfs(field, N);
+    vector<vector<bool>> visit;
+    
+    for (int i = 0; i < N; i++) {
+        visit.push_back(vector<bool> (N));
+    }
+    
+    vector<int> answer;
+    
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if(field[i][j] == 0 || visit[i][j]) {
+                continue;
+            }
+            
+            answer.push_back(dfs(field, visit, N, i, j));
+            
+            /*//중간 결과 출력
+            for (int l = 0; l < N; l++) {
+                for (int m = 0; m < N; m++) {
+                    if (visit[l][m]) {
+                        cout << "T";
+                    }
+                    else {
+                        cout <<"F";
+                    }
+                }
+                cout << endl;
+            }
+            cout << endl;
+            */
+        }
+    }
     
     sort(answer.begin(), answer.end());
     
