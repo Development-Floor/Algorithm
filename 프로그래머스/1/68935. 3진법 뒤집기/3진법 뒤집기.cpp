5fcdb65889ad146a6478fa593d0b2d5332cc97c7
Minwoo <ymw1023@kakao.com>
2025-01-10 09:11:08 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    while (n > 0) {
+        answer = answer * 3 + n % 3;
+        n /= 3;
+    }
+    
+    return answer;
+}
