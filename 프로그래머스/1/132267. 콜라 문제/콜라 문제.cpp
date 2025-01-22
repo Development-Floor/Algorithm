075562b23f00925b89de32fa15c457099437fb95
Minwoo <ymw1023@kakao.com>
2025-01-22 09:11:47 +0900

    [level 1] Title: 콜라 문제, Time: 0.05 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 int solution(int a, int b, int n) {
-    int answer = 0;
+    int colaCount = 0;
     
     while (n >= a) {
         // 콜라 받기
-        answer = answer + (n / a) * b;
+        colaCount = colaCount + (n / a) * b;
         // 빈병 최신화
         n = n % a + (n / a) * b;
     }
     
-    return answer;
+    return colaCount;
 }
