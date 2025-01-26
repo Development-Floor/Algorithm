c5dd052a8d288061ceaef50dfdf63ff163619131
Minwoo <ymw1023@kakao.com>
2025-01-26 15:22:54 +0900

    [level 1] Title: 과일 장수, Time: 23.43 ms, Memory: 34.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int k, int m, vector<int> score)
+{
+    int answer = 0;
+    
+    sort(score.begin(), score.end(), greater<int>());
+    
+    int size = score.size();
+    
+    for (int i = m - 1; i < size; i += m)
+    {
+        answer = answer + m * score[i];
+    }
+    
+    return answer;
+}
