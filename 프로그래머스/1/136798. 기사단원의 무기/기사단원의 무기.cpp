fbd104aa2bc44c80b5aa5aef1a3421bba12de3f6
Minwoo <ymw1023@kakao.com>
2025-01-31 00:22:00 +0900

    [level 1] Title: 기사단원의 무기, Time: 109.16 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
@@ -1,26 +1,41 @@
-#include <string>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
-int solution(int number, int limit, int power) {
+int solution(int number, int limit, int power)
+{
     int answer = 0;
+    int count = 0;
     
-    vector<int> numbers(number + 1);
-    
-    for (int i = 1; i <= number; i++) {
-        for (int j = i; j <= number; j += i) {
-            numbers[j]++;
+    for (int i = 1; i <= number; i++)
+    {
+        count = 0;
+        
+        // 약수의 개수 구하기
+        for (int j = 1; j <= sqrt(i); j++)
+        {
+            if (i % j == 0)
+            {
+                count++;
+                
+                if (j != i / j)
+                {
+                    count++;
+                }
+            }
         }
-    }
-    
-    for (int i = 1; i <= number; i++) {
-        if (numbers[i] > limit) {
+        
+        // 개수가 limit 초과
+        if (count > limit)
+        {
             answer += power;
         }
         
-        else {
-            answer += numbers[i];
+        // 개수가 limit 초과x
+        else
+        {
+            answer += count;
         }
     }
     
