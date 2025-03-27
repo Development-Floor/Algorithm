57d920d905cbb62ad782820e82ce8705be7bd923
Minwoo <ymw1023@kakao.com>
2025-03-27 10:52:47 +0900

    [level 2] Title: 괄호 회전하기, Time: 5.21 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/README.md
+++ 프로그래머스/2/76502. 괄호 회전하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 회전하기 - 76502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502#) 
 
 ### 성능 요약
 
-메모리: 4.02 MB, 시간: 3.59 ms
+메모리: 4.22 MB, 시간: 5.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 27일 10:06:27
+2025년 03월 27일 10:52:45
 
 ### 문제 설명
 
--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
@@ -3,6 +3,93 @@
 
 using namespace std;
 
+// 답은 통과 못 하지만 "({)}" <- 이런거는 통과 가능
+int solution2(string s) {
+    int answer = 0;
+    int sSize = s.size();
+    
+    if (sSize % 2)
+    {
+        return 0;
+    }
+    
+    stack<char> stSmall;
+    stack<char> stMiddle;
+    stack<char> stBig;
+    
+    for (int i = 0; i < sSize; i++)
+    {
+        for (int j = 0; j < sSize; j++)
+        {
+            char k = s[(i + j) % sSize];
+            
+            if (k == '(')
+            {
+                stSmall.push('(');
+            }
+            
+            else if (k == '{')
+            {
+                stMiddle.push('{');
+            }
+            
+            else if (k == '[')
+            {
+                stBig.push('[');
+            }
+            
+            else if (k == ')')
+            {
+                if (stSmall.empty())
+                {
+                    answer--;
+                    break;
+                }
+                stSmall.pop();
+            }
+            
+            else if (k == '}')
+            {
+                if (stMiddle.empty())
+                {
+                    answer--;
+                    break;
+                }
+                stMiddle.pop();
+            }
+            
+            else if (k == ']')
+            {
+                if (stBig.empty())
+                {
+                    answer--;
+                    break;
+                }
+                stBig.pop();
+            }
+        }
+        
+        answer++;
+        
+        while (!stSmall.empty())
+        {
+            stSmall.pop();
+        }
+        
+        while (!stMiddle.empty())
+        {
+            stMiddle.pop();
+        }
+        
+        while (!stBig.empty())
+        {
+            stBig.pop();
+        }
+    }
+
+    return answer;
+}
+
 int solution(string s) {
     int answer = 0;
     int s_len  = s.length();
